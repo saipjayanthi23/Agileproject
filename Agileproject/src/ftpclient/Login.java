4c5154a356550f781bc1be59d8b46fc5577d8981
Samina Khan <skhan2@pdx.edu>
2015-07-06 15:59:05 -0700

    testing commit

---
+++ Agileproject/src/ftpclient/Login.java
@@ -0,0 +1,14 @@
+package ftpclient;
+
+public class Login {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+		System.out.println("Test line");
+	}
+
+}
